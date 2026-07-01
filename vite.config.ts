34bd8aca664d4804bec5f8dd722b5605a738c6b1
abok-cymk <allanabok18@gmail.com>
2026-07-01 19:06:18 +0300

    fix: update base path to match repo name portfolio-allan-cymk

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-website/',
+  base: '/portfolio-allan-cymk/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
